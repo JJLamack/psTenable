7f56348b572437fae36dbe7ceb606a53dc65edf4
Joshua Lamack <jlamack02@nicoletbank.com>
2023-02-24 11:48:49 -0600

    Added build.ps1 Script - makes it easier to install new module Updated module manifiest w/ new build method updated Repository format file

--- build.ps1
+++ build.ps1
@@ -1,3 +1,29 @@
+function Get-ScopedFileLocation {
+    param (
+        [System.IO.FileInfo[]]
+        $Path
+    )
+    $result = @()
+    foreach ($P in $Path) {
+        $result += "$(($P.Directory).Name)\$($P.Name)"
+    }
+    return $result
+}
+
+# Update Format & Types in psd1
+$TypesItems = @(Get-ChildItem -Path .\psTenable\Types -Recurse -Filter "*.types.ps1xml") | Sort-Object Name
+$FormatItems = @(Get-ChildItem -Path .\psTenable\Formats -Recurse -Filter "*.format.ps1xml") | Sort-Object Name
+$PublicItems = @(Get-ChildItem -Path .\psTenable\Public -Recurse -Filter "*.ps1") | Sort-Object Name
+$UtilityItems = @(Get-ChildItem -Path .\psTenable\Utility -Recurse -Filter "*.utility.ps1") | Sort-Object Name
+
+$TypesScopedPath = Get-ScopedFileLocation -Path $TypesItems
+$FormatScopedPath = Get-ScopedFileLocation -Path $FormatItems
+$UtilityScopedPath = Get-ScopedFileLocation -Path $UtilityItems
+$PublicBaseNames = $PublicItems.Basename
+
+Update-ModuleManifest -Path ".\psTenable\psTenable.psd1" -TypesToProcess $TypesScopedPath -FormatsToProcess $FormatScopedPath -ScriptsToProcess $UtilityScopedPath -FunctionsToExport $PublicBaseNames
+
+# Cleanup in memory module and move latest copy to powershell directory to Import
 Remove-Module psTenable -ErrorAction SilentlyContinue
 Copy-Item H:\Projects\psTenable\psTenable\ H:\WindowsPowerShell\Modules\ -Force -Recurse
 Import-Module psTenable
