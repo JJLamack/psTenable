a117b5838f32b7d4758064329a6c3af6ac06c5e5
Joshua Lamack <josh.lamack@gmail.com>
2023-03-05 21:06:35 -0600

    add get current org switch query

--- psTenable/Public/Get-TenableSCOrganization.ps1
+++ psTenable/Public/Get-TenableSCOrganization.ps1
@@ -13,21 +13,33 @@ function Get-TenableSCOrganization {
 
     Filters results returned based on the Properties. Default displays all properities that can be returned from the Organization API endpoint except for those that can be quiered from other endpoints.
 
+    .PARAMETER CurrentOrganization
+
+    Filters results to only show the current organization that the user credentials provided are a part of
+
     #>
     [cmdletBinding(DefaultParameterSetName = 'Default')]
     param (
         [Parameter(ParameterSetName = 'Default', Mandatory = $false, Position = 0, ValueFromPipeline = $true)]
         [Alias('Id', 'UUID')]
         $Organization,
-        [Parameter(Mandatory = $false)]
+        [Parameter(ParameterSetName = 'Default', Mandatory = $false)]
         [ArgumentCompletions("id", "uuid", "name", "description", "email", "address", "city", "state", "country", "phone", "fax", "ipInfoLinks", "zoneSelection", "restrictedIPs", "vulnScoreLow", "vulnScoreMedium", "vulnScoreHigh", "vulnScoreCritical", "vulnScoringSystem", "createdTime", "modifiedTime", "passwordExpires", "passwordExpiration", "userCount", "lces", "repositories", "zones", "nessusManagers", "pubSites", "ldaps")]
-        $Properties
+        $Properties,
+        [Parameter(ParameterSetName = 'Current', Mandatory = $true)]
+        [switch]
+        $CurrentOrganization
     )
     begin {
         $Endpoint = "organization"
     }
     process {
-        $result = Get-TenableSC -Resource $Endpoint -Properties $Properties -Key $Organization
+        if ($CurrentOrganization) {
+            $result = Invoke-TenableSCMethod -Endpoint "currentOrganization"
+        }
+        else { 
+            $result = Get-TenableSC -Resource $Endpoint -Properties $Properties -Key $Organization 
+        }
         return $result
     }
 }
