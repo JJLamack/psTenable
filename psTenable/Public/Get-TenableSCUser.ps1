559076572e856bb2bc750e2f687abc6b528c949c
Joshua Lamack <josh.lamack@gmail.com>
2023-03-05 20:46:20 -0600

    added current user query to Get User

--- psTenable/Public/Get-TenableSCUser.ps1
+++ psTenable/Public/Get-TenableSCUser.ps1
@@ -3,7 +3,7 @@ function Get-TenableSCUser {
     
     .SYNOPSIS
 
-    Gets a list of Users
+    Gets a list of Users from Tenable Security Center. If CurrentUser flag is set then only gets the current user.
 
     .PARAMETER User
 
@@ -14,20 +14,28 @@ function Get-TenableSCUser {
     Filters results returned based on the Properties.
 
     #>
-    [cmdletBinding(DefaultParameterSetName='Default')]
+    [cmdletBinding(DefaultParameterSetName = 'Default')]
     param (
         [Parameter(ParameterSetName = 'Default', Mandatory = $false, Position = 0, ValueFromPipeline = $true)]
         [Alias('Id', 'UUID')]
         $User,
         [Parameter(Mandatory = $false)]
-        [ArgumentCompletions("id","uuid","username","firstname","lastname","status","role","title","email","address","city","state","country","phone","fax","createdTime","modifiedTime","lastLogin","lastLoginIP","mustChangePassword","passwordExpires","passwordExpiration","passwordExpirationOverride","passwordSetDate","locked","failedLogins","authType","fingerprint","password","description","canUse","canManage","managedUsersGroups","managedObjectsGroups","preferences","ldaps","ldapUsername","linkedUsers","parent","responsibleAsset","group")]
-        $Properties
+        [ArgumentCompletions("id", "uuid", "username", "firstname", "lastname", "status", "role", "title", "email", "address", "city", "state", "country", "phone", "fax", "createdTime", "modifiedTime", "lastLogin", "lastLoginIP", "mustChangePassword", "passwordExpires", "passwordExpiration", "passwordExpirationOverride", "passwordSetDate", "locked", "failedLogins", "authType", "fingerprint", "password", "description", "canUse", "canManage", "managedUsersGroups", "managedObjectsGroups", "preferences", "ldaps", "ldapUsername", "linkedUsers", "parent", "responsibleAsset", "group")]
+        $Properties,
+        [Parameter(ParameterSetName = 'CurrentUser', Mandatory = $true)]
+        [switch]
+        $CurrentUser
     )
     begin {
         $Endpoint = "user"
     }
     process {
-        $result = Get-TenableSC -Resource $Endpoint -Properties $Properties -Key $User
+        if ($CurrentUser) {
+            $result = Get-TenableSC -Resource "currentUser" -Properties $Properties
+        }
+        else {
+            $result = Get-TenableSC -Resource $Endpoint -Properties $Properties -Key $User
+        }
         return $result
     }
 }
